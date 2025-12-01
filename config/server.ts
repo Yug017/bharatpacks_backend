d45d4a53139232057dabef0458b7bd0981605252
mtplweb <mtplweb25@gmail.com>
2025-12-01 22:51:33 +0530

    Initial commit - Strapi project

--- config/server.ts
+++ config/server.ts
@@ -1,17 +1,7 @@
 export default ({ env }) => ({
-  url: env("PUBLIC_URL", "https://backend.bharatpacks.com/"),
-  host: "0.0.0.0",
-  port: env.int("PORT", 1337),
-
-  // REQUIRED (Fixes your error)
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS", [
-      "key1",
-      "key2",
-      "key3",
-      "key4",
-    ]),
+    keys: env.array('APP_KEYS'),
   },
-
-  proxy: true,
 });
